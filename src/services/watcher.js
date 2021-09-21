ef56bcb64cc1fdd6fac190b49e9ac24ddd308fc0
zhijie <guszhiwe@student.gu.se>
2021-09-21 14:39:46 +0200

    update availability

---
+++ src/services/watcher.js
@@ -0,0 +1,15 @@
+const {Publisher} = require( "./publisher");
+const fs = require("fs");
+
+class Watcher {
+    constructor() {
+    }
+    watch(file) {
+        fs.watchFile(file, () => {
+            let publisher = new Publisher();
+            publisher.publishTimeSlots(file);
+        })
+    }
+}
+
+module.exports.Watcher = Watcher;
